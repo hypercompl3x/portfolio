0875a87099f964879a9cfb85e634ce819f3567d5
hypercompl3x <45500254+hypercompl3x@users.noreply.github.com>
2024-05-22 13:15:31 +0100

    use new images with no background

--- src/components/pageComponents/projects/ProjectCard/index.tsx
+++ src/components/pageComponents/projects/ProjectCard/index.tsx
@@ -3,9 +3,11 @@ import Image from 'next/image';
 import type { Project } from '@/lib/types';
 
 const ProjectCard: FC<Project> = ({ description, image, link, title }) => (
-	<div className='flex max-sm:flex-col max-sm:text-center border-primary border rounded-md gap-x-6 max-sm:items-center gap-y-2 overflow-hidden max-sm:p-2.5'>
-		<Image src={image} className='w-44 object-cover max-sm:rounded-md' alt={`${title}-image`} />
-		<div className='space-y-1 sm:space-y-0.5 sm:p-2.5'>
+	<div className='flex max-sm:flex-col max-sm:text-center border-primary border rounded-md gap-x-6 max-sm:items-center gap-y-1 overflow-hidden'>
+		<div className='bg-primary shrink-0 flex items-center justify-center p-5 max-sm:w-full'>
+			<Image src={image} className='w-36' alt={`${title}-image`} />
+		</div>
+		<div className='space-y-1 p-2.5'>
 			{link ? (
 				<a
 					target='_blank'
@@ -15,7 +17,7 @@ const ProjectCard: FC<Project> = ({ description, image, link, title }) => (
 					{title}
 				</a>
 			) : (
-				<div className='flex items-center gap-x-3'>
+				<div className='flex items-center gap-x-3 max-sm:justify-center'>
 					<div className='text-2xl font-bold'>{title}</div>
 					<div className='font-semibold'>{'(In Progress)'}</div>
 				</div>
